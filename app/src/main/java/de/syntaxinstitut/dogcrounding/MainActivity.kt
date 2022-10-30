a92355cf9e0185c91b535fa27e3ba52206611e5c
AppNewbie86 <nobody_is_perfect22@outlook.de>
2022-10-30 17:34:00 +0100

    update3.0

--- app/src/main/java/de/syntaxinstitut/dogcrounding/MainActivity.kt
+++ app/src/main/java/de/syntaxinstitut/dogcrounding/MainActivity.kt
@@ -1,28 +1,15 @@
 package de.syntaxinstitut.dogcrounding
 
-import android.app.ProgressDialog
-import android.content.Context
-import android.content.Intent
-import android.media.MediaPlayer
-import android.net.Uri
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.TransitionDrawable
 import android.os.Bundle
 import android.widget.Button
-import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import de.syntaxinstitut.dogcrounding.adapter.MessageAdapter
-import de.syntaxinstitut.dogcrounding.data.Message
 import de.syntaxinstitut.dogcrounding.databinding.ActivityMainBinding
-import de.syntaxinstitut.dogcrounding.utils.BotResponse
-import de.syntaxinstitut.dogcrounding.utils.Constants.OPEN_GOOGLE
-import de.syntaxinstitut.dogcrounding.utils.Constants.OPEN_SEARCH
-import de.syntaxinstitut.dogcrounding.utils.Constants.RECEIVE_ID
-import de.syntaxinstitut.dogcrounding.utils.Constants.SEND_ID
-import de.syntaxinstitut.dogcrounding.utils.Time
-import kotlinx.coroutines.*
 
 /**
  * Main Activity, dient als Einstiegspunkt für die App
@@ -34,9 +21,6 @@ class MainActivity : AppCompatActivity() {
     /** Bindet das XML-View mit der Klasse um auf die Elemente zugreifen zu können */
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
-
-
-
 
 
     /* -------------------- Lifecycle -------------------- */
@@ -54,6 +38,7 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.hide()
 
 
+    }
 
-}}
+}
 
